9bd54f1845c47bd3b1ce5ae0756143b3f36a38a0
davekg8 <davidegbakou@gmail.com>
2025-06-03 16:44:28 +0200

    update admin interface

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,16 +1,16 @@
 from django.contrib import admin
-
+from unfold.admin import ModelAdmin
 from catalog.models import Product, Category
 
 
 @admin.register(Product)
-class ProductAdmin(admin.ModelAdmin):
+class ProductAdmin(ModelAdmin):
     list_display = ('name', 'price', 'category',)
     list_filter = ('category',)
     readonly_fields = ('image_preview',)
 
 @admin.register(Category)
-class CategoryAdmin(admin.ModelAdmin):
+class CategoryAdmin(ModelAdmin):
     list_display = ('name',)
     readonly_fields = ('image_preview',)
 
