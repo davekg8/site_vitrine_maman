651eab24d430283c74d6bfe146fb8077a22cb1ae
davekg8 <davidegbakou@gmail.com>
2025-06-27 13:09:44 +0200

    Update with whatsapp

--- pagne_elegance_project/urls.py
+++ pagne_elegance_project/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings # Importez settings
 from django.conf.urls.static import static # Importez static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('christfashion/', admin.site.urls),
     path('', include('catalog.urls'))
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
